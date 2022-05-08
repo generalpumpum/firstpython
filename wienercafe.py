820868b54557a0ffdfb33ea1d4dadf4e0de3861e
Kaiser <elgeneraldelpumpum@gmail.com>
2022-05-08 21:36:32 +0200

    first commit

---
+++ wienercafe.py
@@ -0,0 +1,10 @@
+#my first steps in Python the idea is to create strings, definition of variables 
+
+
+print ("Gross Gott Mein Name ist Gunther ich bin Ihrer Barista ")
+name = input("Wie heissen Sie  \n") # variable defenition
+print ("ich freue mich dass "+ name+ " mit uns Heute sind ")
+carta= (" Capuccino, Verlänger und  Melange") # ariable defenition 
+print ("Ich möcheten Sie bestellen" + carta +" was möchten Sie "+ name)
+cliente= input()
+print("ihrer bestelung  "+ cliente + "  wird in 5 minuten fertig")
