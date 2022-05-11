dc117b51a09872e24d34c54d14eb843da7a4620c
Kaiser <elgeneraldelpumpum@gmail.com>
2022-05-11 20:46:44 +0200

    add wienercafe.py

--- wienercafe.py
+++ wienercafe.py
@@ -1,10 +1,27 @@
-#my first steps in Python the idea is to create strings, definition of variables 
+#my first step in Python the idea is to create strings, definition 
+# of variables, if and else statements and if statements embedded  
+#there is two things about our barista first he has a f-word up German 
+#and he is a hardcore vegan 
 
-
-print ("Gross Gott Mein Name ist Gunther ich bin Ihrer Barista ")
+print ("Grüßs Gott Mein Name ist Gunther ich bin Ihrer Barista ")
 name = input("Wie heissen Sie  \n") # variable defenition
-print ("ich freue mich dass "+ name+ " mit uns Heute sind ")
-carta= (" Capuccino, Verlänger und  Melange") # ariable defenition 
-print ("Ich möcheten Sie bestellen" + carta +" was möchten Sie "+ name)
+print ("ich freue mich dass "+ name+ " mit uns Heute Sind ")
+carta= (" Capuccino, Verlänger und  Espresso") # variable defenition 
+print ("Ich möcheten Ihnen anbieten\n" + carta +" was möchten Sie "+ name)
 cliente= input()
-print("ihrer bestelung  "+ cliente + "  wird in 5 minuten fertig")
+if cliente == "Capuccino" :
+    leche = input("Möchten Sie normal Milch oder Hafer  Milch  \n")
+    if leche == "normal": #this is a if function embedded in another if function
+        print(" es tut mir  leid wir haben keine Milch,\n wir sind ein Veganes Geschäft,  \n kein cafe für Sie Barbaric Milchtrinker")
+        quit()
+    else:
+        print("ihrer bestelung  "+ name  + " mit Hafer milch wird in 5 minuten fertig")
+if cliente == "Verlänger" :
+   
+      print("ihrer bestelung  "+ name + "  wird in 5 minuten fertig")
+      quit()
+if cliente =="Espresso" :
+    print("ihrer bestelung  "+ name + "  wird in 5 minuten fertig")
+    quit()
+else :
+    print("es tut mir leid wir haben nur"+ carta)
